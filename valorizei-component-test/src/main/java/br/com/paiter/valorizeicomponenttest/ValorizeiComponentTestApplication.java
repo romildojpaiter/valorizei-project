aeaa69fedf161ce90b3226e043246fea3e7d9e3d
Romildo Paiter <romildopaiter@gmail.com>
2020-05-12 16:58:21 -0300

    Inclusão deo projeto valorizei-component-test

---
+++ valorizei-component-test/src/main/java/br/com/paiter/valorizeicomponenttest/ValorizeiComponentTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.paiter.valorizeicomponenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorizeiComponentTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorizeiComponentTestApplication.class, args);
+    }
+
+}
---
+++ valorizei-component-test/src/test/java/br/com/paiter/valorizeicomponenttest/ValorizeiComponentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paiter.valorizeicomponenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorizeiComponentTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
